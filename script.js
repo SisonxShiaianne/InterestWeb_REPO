ecd17f9effa422c03ddd4d7ea1ca6b557a96dbd3
Shiaianne <shiaiannesisonn@gmail.com>
2023-10-09 23:51:51 +0800

    Created the first draft

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*==================== NAV BAR ====================*/ 
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu').onclick = () =>{
@@ -7,3 +8,49 @@ document.querySelector('#menu').onclick = () =>{
 window.onscroll = () =>{
     navbar.classList.remove('active');
 }
+
+var swiper = new Swiper(".books-slider", {
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+      1440: {
+        slidesPerView: 3,
+      },
+      2560: {
+        slidesPerView: 3,
+      },
+    },
+});
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
